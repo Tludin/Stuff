42908a623417acd51a92cadfbb39c41548a1a3ed
Stephen Ludin <sludin@ludin.org>
2019-10-22 20:08:20 -0700

    Converted tabs to spaces.  Tabs are evil

--- AragornVsOrcs.java
+++ AragornVsOrcs.java
@@ -1,4 +1,3 @@
-
 /**
  * Aragorn is fighting Morannon orcs at the Black Gate.
  * He can fight any number of orcs at a time, just cahnge the numbers, right now he will fight one at a time.
@@ -8,94 +7,94 @@
  */
 public class AragornVsOrcs
 {
-	public static void main(String[] args)
-	{
-		int totalOrcsKilled = 0;
-		int n = 10000;
-		int maxOrcs = 0;
-		int minOrcs = 0;
+  public static void main(String[] args)
+  {
+    int totalOrcsKilled = 0;
+    int n = 10000;
+    int maxOrcs = 0;
+    int minOrcs = 0;
+    
+    for(int i=0; i<n; i++)
+    {
+      int orcCasualty = 0;
+      int aragornWounds = 3;
+      boolean aragornAlive = true;
 
-		for(int i=0; i<n; i++)
-		{
-			int orcCasualty = 0;
-			int aragornWounds = 3;
-			boolean aragornAlive = true;
+      while (aragornAlive == true)
+      {
+        int numAragornDice = 3;
+        int aragornDice[] = new int[numAragornDice];
 
-			while (aragornAlive == true)
-			{
-				int numAragornDice = 3;
-				int aragornDice[] = new int[numAragornDice];
-
-				for ( int j = 0; j < numAragornDice; j++ )
-				{
-					aragornDice[j] = (int)(6*Math.random()+1);
-				}
-
-
-				int nOrcs = 1;
-				int orcDice[] = new int[nOrcs];
-
-				for ( int j = 0; j < nOrcs; j++ )
-				{
-					orcDice[j] = (int)(6*Math.random()+1);
-				}
+        for ( int j = 0; j < numAragornDice; j++ )
+        {
+          aragornDice[j] = (int)(6*Math.random()+1);
+        }
 
 
-				
-				int aragornHighest = getMax(aragornDice);
-				int orcHighest = getMax(orcDice);
+        int nOrcs = 1;
+        int orcDice[] = new int[nOrcs];
 
-				if (aragornHighest >= orcHighest){
-					for (int j=1; j<=aragornDice.length; j++){
-						int aragornHit = (int)(6*Math.random()+1);
-
-						if(aragornHit >= 4) {
-							orcCasualty++;
-						}
-					}
-				}
-				
-				if (orcHighest > aragornHighest){
-					for (int j=1; j<=orcDice.length; j++){
-						int orcHit = (int)(6*Math.random()+1);
-
-						if (orcHit >= 5) {
-							aragornWounds--;
-						}
-					}
-				}
-				
-				if(aragornWounds <= 0){
-					aragornAlive = false;
-					// System.out.println("Total Orcs Killed: " + orcCasualty);
-					totalOrcsKilled = totalOrcsKilled + orcCasualty;
-					if(orcCasualty > maxOrcs){
-						maxOrcs = orcCasualty;
-					}
-
-					if(orcCasualty < minOrcs){
-						minOrcs = orcCasualty;
-					}
-				}
-			}
-		}
-		
-		int orcAverage = totalOrcsKilled / n;
-
-		System.out.println("Average number of orcs killed: " + orcAverage);
-		System.out.println("Maximum number of orcs killed: " + maxOrcs);
-		System.out.println("Minimum number of orcs killed: " + minOrcs);
-	}
+        for ( int j = 0; j < nOrcs; j++ )
+        {
+          orcDice[j] = (int)(6*Math.random()+1);
+        }
 
 
-	public static int getMax(int[] dieArray){
-		int maxValue = dieArray[0];
+        
+        int aragornHighest = getMax(aragornDice);
+        int orcHighest = getMax(orcDice);
 
-		for(int val : dieArray )
-		{
-			maxValue = (val > maxValue ? val : maxValue);
-		}
+        if (aragornHighest >= orcHighest){
+          for (int j=1; j<=aragornDice.length; j++){
+            int aragornHit = (int)(6*Math.random()+1);
 
-		return maxValue;
-	}
+            if(aragornHit >= 4) {
+              orcCasualty++;
+            }
+          }
+        }
+        
+        if (orcHighest > aragornHighest){
+          for (int j=1; j<=orcDice.length; j++){
+            int orcHit = (int)(6*Math.random()+1);
+
+            if (orcHit >= 5) {
+              aragornWounds--;
+            }
+          }
+        }
+        
+        if(aragornWounds <= 0){
+          aragornAlive = false;
+          // System.out.println("Total Orcs Killed: " + orcCasualty);
+          totalOrcsKilled = totalOrcsKilled + orcCasualty;
+          if(orcCasualty > maxOrcs){
+            maxOrcs = orcCasualty;
+          }
+
+          if(orcCasualty < minOrcs){
+            minOrcs = orcCasualty;
+          }
+        }
+      }
+    }
+    
+    int orcAverage = totalOrcsKilled / n;
+
+    System.out.println("Average number of orcs killed: " + orcAverage);
+    System.out.println("Maximum number of orcs killed: " + maxOrcs);
+    System.out.println("Minimum number of orcs killed: " + minOrcs);
+  }
+
+
+  public static int getMax(int[] dieArray){
+    int maxValue = dieArray[0];
+
+    for(int val : dieArray )
+    {
+      maxValue = (val > maxValue ? val : maxValue);
+    }
+
+    return maxValue;
+  }
 }
