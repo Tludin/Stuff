408a6ca76bf2dd53119b43ec27d70f74fc2cf2e9
Tludin <55504609+Tludin@users.noreply.github.com>
2020-06-28 17:09:20 -0700

    Add files via upload

--- MapGenerator.js
+++ MapGenerator.js
@@ -6,16 +6,21 @@
 var http = require('http');
 var url = require('url');
 
-console.log(url.URL);
 var mapWidth = 200;
 var mapHeight = 100;
 var map = [mapHeight];
 
 http.createServer(function (req, res) {
-	if(url === "localhost:8080/map")
+	var fullUrl = req.url;
+	console.log(fullUrl);
+	var q = url.parse(fullUrl, true);
+	if(q.pathname === "/map")
 	{
+		var qData = q.query;
+		mapWidth = qData.width;
+		mapHeight =  qData.height;
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		var continent = generateMap();
+		var continent = buildMap();
 		res.write(continent);
 		res.end();
 	}
@@ -27,7 +32,38 @@ http.createServer(function (req, res) {
 	}
 }).listen(8080);
 
-function generateMap()
+
+function buildMap()
+{
+	var i = 0;
+	var j = 0;
+	for(i = 0; i < mapHeight; i++)
+	{
+		map[i] = [mapWidth];
+		for(j = 0; j < mapWidth; j++)
+		{
+			map[i][j] = "X";
+		}
+	}
+	
+	buildContinent();
+	buildMountains(250);
+	var mapString = "";
+	for(i = 0; i < mapHeight; i++)
+	{
+		for(j = 0; j < mapWidth; j++)
+		{
+			mapString += map[i][j];
+		}
+		mapString += "\n";
+	}
+	var mapBody = "<pre>\n" + mapString + "</pre>\n";
+	return mapBody;
+	
+}
+
+
+function buildContinent()
 {
 
 	var i = 0;
@@ -41,15 +77,6 @@ function generateMap()
 	var param = 0;
 	var check = 0;
 	var result = 0;
-	for(i = 0; i < mapHeight; i++)
-	{
-		map[i] = [mapWidth];
-		for(j = 0; j < mapWidth; j++)
-		{
-			map[i][j] = "X";
-		}
-	}
-	j = 0;
 	//console.log("Check");
 	for(i = 0; i < highI; i++)
 	{
@@ -156,50 +183,153 @@ function generateMap()
 		i--;
 	    highI = i;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	var mapString = "";
-	for(i = 0; i < mapHeight; i++){
-		for(j = 0; j < mapWidth; j++){
-			mapString += map[i][j];
-		}
-		mapString += "\n";
+		
+}
+
+
+function buildMountains(n)
+{
+	console.log('Adding Mountains');
+	var i = 0;
+	var j = 0;
+	var lowI = -1;
+	var lowJ = 0;
+	var highI = mapHeight;
+	var highJ = mapWidth;
+	var prob = 55;
+	var param = 0;
+	var check = 0;
+	var result = 0;
+	//console.log("Check");
+	for(i = 0; i < highI; i++)
+	{
+		 result = Math.floor((Math.random()*100+1));
+		 check = nextTo(j,i,"∆");
+         if(map[i][j].equals("=")){
+             if(result < n+check*prob){
+                 map[i][j] = "∆";
+             }
+         }
+         param++;
 	}
-	var mapBody = "<pre>\n" + mapString + "</pre>\n";
-	return mapBody;
+	
+	i--;
+    highI = i;
+    
+    
+	while(param < mapWidth*mapHeight)
+	{
+		for(j = lowJ + 1; j < highJ; j++)
+		{
+			 result = Math.floor((Math.random()*100+1));
+			 check = nextTo(j,i,"∆");
+	         if(map[i][j].equals("=")){
+	             if(result < n+check*prob){
+	                 map[i][j] = "∆";
+	             }
+	         }
+	         param++;
+		}
+		
+		j--;
+	    highJ = j;
+	    
+	    
+	    for(i = highI - 1; i > lowI; i--)
+		{
+	    	 result = Math.floor((Math.random()*100+1));
+	    	 check = nextTo(j,i,"∆");
+	         if(map[i][j].equals("=")){
+	             if(result < n+check*prob){
+	                 map[i][j] = "∆";
+	             }
+	         }
+	         param++;
+		}
+	    
+	    i++;
+	    lowI = i;
+	    
+	    
+	    for(j = highJ - 1; j > lowJ; j--)
+		{
+	    	 result = Math.floor((Math.random()*100+1));
+	    	 check = nextTo(j,i,"∆");
+	         if(map[i][j].equals("=")){
+	             if(result < n+check*prob){
+	                 map[i][j] = "∆";
+	             }
+	         }
+	         param++;
+		}
+	    
+	    j++;
+	    lowJ = j;
+	    
+	    
+	    for(i = lowI + 1; i < highI; i++)
+		{
+	    	 result = Math.floor((Math.random()*100+1));
+	    	 check = nextTo(j,i,"∆");
+	         if(map[i][j].equals("=")){
+	             if(result < n+check*prob){
+	                 map[i][j] = "∆";
+	             }
+	         }
+	         param++;
+		}
+		i--;
+	    highI = i;
+	}
 	
 }
 
-function nextTo(y, x, c){
+
+function buildMountains()
+{
+	
+	
+	
+}
+
+
+function nextTo(y, x, c)
+{
 	var count = 0;
-	if(x < mapHeight - 1){
-		if((map[x+1][y]) === c){
+	if(x < mapHeight - 1)
+	{
+		if((map[x+1][y]) === c)
+		{
 			count++;
 		}
 	}
-	if(x > 0){
-		if((map[x-1][y]) === c){
+	if(x > 0)
+	{
+		if((map[x-1][y]) === c)
+		{
 			count++;
 		}
 	}
-	if(y < mapWidth - 1){
-		if((map[x][y+1]) === c){
+	if(y < mapWidth - 1)
+	{
+		if((map[x][y+1]) === c)
+		{
 			count++;
 		}
 	}
-	if(y > 0){
-		if((map[x][y-1]) === c){
+	if(y > 0)
+	{
+		if((map[x][y-1]) === c)
+		{
 			count++;
 		}
 	}
 	return count;
 }
+
+
+
+
 
 function test()
 {
